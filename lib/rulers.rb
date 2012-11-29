9c4f8262998e685a3f9c99d629d8f0aac32db54c
Noah Gibbs <noah_gibbs@yahoo.com>
2012-11-28 23:43:30 -0800

    Don't show errors for favicon.ico

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,11 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)

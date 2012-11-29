229059e71f4fb6f0f2894b61e0cd112e86cebe9f
Noah Gibbs <noah_gibbs@yahoo.com>
2012-11-28 23:40:06 -0800

    Add real routing, and increment version

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,23 @@
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end

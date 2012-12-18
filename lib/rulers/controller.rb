212fe20f10097c72ca2443b5824f26b9314ca440
Noah Gibbs <noah_gibbs@yahoo.com>
2012-12-18 03:15:26 -0800

    Add chapter 6 content.

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require "erubis"
 require "rulers/file_model"
+require "rack/request"
 
 module Rulers
   class Controller
@@ -11,6 +12,28 @@ module Rulers
 
     def env
       @env
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status=200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response # Only for Rulers
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
     end
 
     def render(view_name, locals = {})

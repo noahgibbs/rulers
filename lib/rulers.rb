876929031ce966f37e8b5e109fcea4ba6a6f418c
Washington Luiz <huoxito@gmail.com>
2013-04-01 00:49:55 -0300

    Update Application#call to use get_rack_app

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -13,8 +13,7 @@ module Rulers
           {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, act = get_controller_and_action(env)
-      rack_app = klass.action(act)
+      rack_app = get_rack_app(env)
       rack_app.call(env)
     end
   end
